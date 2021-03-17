89561201855e619e1604982ec0fcb97e8647d6ae
Han <lucirhk@gmail.com>
2021-03-17 23:59:07 +0900

    Component: footer

---
+++ src/components/footer/footer.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import styles from './footer.module.css';
+
+const Footer = (props) => (
+    <footer className={styles.footer}>
+      <span className={styles.description}>Make your memories</span>
+    </footer>
+  );
+
+export default Footer;
