9ab28ab4de8c9904320ffd26da3293018eefc338
Han <lucirhk@gmail.com>
2021-03-18 23:30:28 +0900

    Add Auth login

--- src/components/login/login.jsx
+++ src/components/login/login.jsx
@@ -2,19 +2,28 @@ import React from 'react';
 import Header from '../header/header';
 import Footer from '../footer/footer';
 
+const Login = ({ authService }) => {
+	const handleLogin = event => {
+		authService //
+			.login(event.currentTarget.textContent)
+			.then(console.log);
+	};
 
-const Login = (props) => {
-  const handleLogin = () => {
-
-  }
-  return (
-  <>
-    <Header></Header>
-      <button onClick={handleLogin}>Google</button>
-      <button onClick={handleLogin}>Github</button>
-    <Footer></Footer>
-  </>
-  )
+	return (
+		<section>
+			<Header></Header>
+			<h1>Login</h1>
+			<ul>
+				<li>
+					<button onClick={handleLogin}>Google</button>
+				</li>
+				<li>
+					<button onClick={handleLogin}>Github</button>
+				</li>
+			</ul>
+			<Footer></Footer>
+		</section>
+	);
 };
 
 export default Login;
