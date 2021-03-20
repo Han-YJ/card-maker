9076c835cc89e9c8b44b0ebdf41b255ad658aeea
Han <lucirhk@gmail.com>
2021-03-20 21:16:48 +0900

    Add Router: login

--- src/app.jsx
+++ src/app.jsx
@@ -1,13 +1,20 @@
 import React from 'react';
-import './app.css';
+import styles from './app.module.css';
 import Login from './components/login/login';
+import { BrowserRouter, Route, Switch} from 'react-router-dom';
 
-function App({authService}) {
-  return (
-    <>
-      <Login authService={authService} />
-    </>
-  );
+function App ({authService}) {
+  return <BrowserRouter>
+    <Switch>
+      <Route path={['/', '/login']} exact >
+        <div className={styles.app}>
+          <Login authService={authService} />
+        </div>
+      </Route>
+    </Switch>    
+  </BrowserRouter>
+    
+  
 }
 
 export default App;
