5061f540d85a86ce89dc793f06d926da842d0d7e
Han <lucirhk@gmail.com>
2021-03-22 21:28:55 +0900

    Add login and logout functionality

--- src/app.jsx
+++ src/app.jsx
@@ -1,20 +1,24 @@
 import React from 'react';
 import styles from './app.module.css';
 import Login from './components/login/login';
-import { BrowserRouter, Route, Switch} from 'react-router-dom';
+import { BrowserRouter, Route, Router, Switch } from 'react-router-dom';
+import Maker from './components/maker/maker';
 
-function App ({authService}) {
-  return <BrowserRouter>
-    <Switch>
-      <Route path={['/', '/login']} exact >
-        <div className={styles.app}>
-          <Login authService={authService} />
-        </div>
-      </Route>
-    </Switch>    
-  </BrowserRouter>
-    
-  
+function App({ authService }) {
+	return (
+		<div className={styles.app}>
+			<BrowserRouter>
+				<Switch>
+					<Route path={['/', '/login']} exact>
+						<Login authService={authService} />
+					</Route>
+						<Route path={'/maker'}>
+							<Maker authService={authService} />
+						</Route>
+				</Switch>
+			</BrowserRouter>
+		</div>
+	);
 }
 
 export default App;
