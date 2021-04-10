bb8fac4b4649ab4c6d5b9c0972b91f76c14259f8
Han <lucirhk@gmail.com>
2021-04-10 21:23:15 +0900

    Use firebase realtime database - syncCards

--- src/components/maker/maker.jsx
+++ src/components/maker/maker.jsx
@@ -16,6 +16,22 @@ const Maker = ({ FileInput, authService, cardRepository }) => {
 		authService.logout();
 	};
 
+  useEffect(() => {
+    if (!userId) {
+      return ;
+    }
+    const stopSync = cardRepository.syncCards(userId, cards => {
+      //1. component가 mount되면 cardRepository의 syncCards가 먼저 호출
+      setCards(cards);
+      //2. syncCards의 함수가 수행(여기서는 cardRepository의 ref.on)
+      //3. 마지막 arrow함수인 ref.off() 리턴되어 stopSync에 할당(수행은 x)
+      //4. return된 stopSync는 아래 return값이 되고 함수가 unmount되었을때 실행 => ref.off()가 실행
+    })
+    return () => stopSync();
+    //useEffect에서 return 하게 되면 함수가 unmount되었을때 알아서 호출해준다 => 리소스나 메모리를 정리하는일을 해줄수 있다
+    //useEffect의 return에서는 항상 function을 리턴해주어야 한다
+  }, [userId])
+
 	useEffect(() => {
 		authService.onAuthChange((user) => {
 			if (user) {
